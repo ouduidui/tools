e89afc0a0d4ad85f698584aea35cd7fe758e7ed4
oushihao <oushihao97@163.com>
2021-12-04 21:49:16 +0800

    ci: 后端打包脚本

--- packages/server/deploy.sh
+++ packages/server/deploy.sh
@@ -6,6 +6,6 @@ cd ${basepath};
 git checkout .;
 git pull;
 
-docker build . -t ouduidui/web-crawler-node;
+docker build . -t ouduidui/tool-egg-server;
 
-docker run -p 7001:7001 -d ouduidui/web-crawler-node;
+docker run -p 7001:7001 -d ouduidui/tool-egg-server;
