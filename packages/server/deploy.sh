acf26e27171a7e66803f97d7788493440f5c809e
oushihao <oushihao97@163.com>
2021-12-04 21:14:30 +0800

    ci: 后端打包脚本

---
+++ packages/server/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+basepath=$(cd `dirname $0`;pwd);
+cd ${basepath};
+
+git pull;
+
+docker build . -t ouduidui/web-crawler-node;
+
+docker run -p 1202:1202 -d ouduidui/web-crawler-node;
