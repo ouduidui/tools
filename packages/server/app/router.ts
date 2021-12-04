903a5e5f1c1f178e26ad3f08384f73589961ca8a
oushihao <oushihao97@163.com>
2021-12-04 18:18:49 +0800

    feat: 实现获取视频评论列表

--- packages/server/app/router.ts
+++ packages/server/app/router.ts
@@ -6,5 +6,6 @@ export default (app: Application) => {
   router.get('/', controller.home.index);
 
   // 通过哔哩哔哩用户ID获取视频列表
-  router.get('/api/crawler/bili-bili/video-list/:id', controller.biliBili.videoList);
+  router.get('/api/crawler/bili-bili/video-list/:uid', controller.biliBili.videoList);
+  router.get('/api/crawler/bili-bili//comments-list/:vid', controller.biliBili.commentList);
 };
--- packages/server/typings/app/service/biliBili.d.ts
+++ packages/server/typings/app/service/biliBili.d.ts
@@ -6,4 +6,29 @@ declare module 'biliBili' {
     comment: string,
     created: number,
   }
+
+  interface CommentListResponse {
+    pagination: PaginationType,
+    comments: CommentType[]
+  }
+
+  interface PaginationType {
+    page: number,
+    limit: number,
+    pages: number,
+    total: number
+  }
+
+  interface CommentType {
+    member: {
+      uid: string,
+      name: string,
+      sex: string,
+      level: number
+    },
+    content: string,
+    created: number,
+    like: number,
+    reply: number
+  }
 }
