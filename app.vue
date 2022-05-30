7aca63ce863096b9630d9d4286d4aab6087304f1
oushihao <oushihao@bytedance.com>
2022-05-30 15:04:52 +0800

    feat: icons
    Change-Id: I37c09fb66cce3354198f46928696724f7e2c5ead

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Tools',
+  link: [
+    {
+      rel: 'icon', type: 'svg', href: '/favicon.svg',
+    },
+  ],
+})
+</script>
+
 <template>
   <NuxtPage />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 const navs = [
-  { path: 'bilibili', label: '哔哩哔哩' },
+  { path: '/tools/bilibili', label: '哔哩哔哩' },
 ]
 </script>
 
 <template>
-  <div h-screen flex="~ col" items-center justify-center>
+  <div h-screen flex="~ col" mb-10 items-center justify-center>
+    <p
+      i-mdi-tools
+      w-15
+      h-15
+      opacity-80
+      mb-10
+    />
     <router-link
       v-for="nav in navs"
       :key="nav.path"
