6992cf8ecf54bf147076b81edbc352514c1b1916
Dewey Ou <oushihao97@gmail.com>
2022-06-03 21:20:56 +0800

    feat: axios interceptor

--- server/utils/request.ts
+++ server/utils/request.ts
@@ -13,12 +13,22 @@ type RequestSuccessReturn = [
   successRes: null | { status: number; data: unknown },
 ]
 
+axios.interceptors.response.use(
+  res => res,
+  (err) => {
+    if (err && err.response && err.response.status)
+      return Promise.resolve(err.response)
+    else
+      return Promise.reject(err)
+  },
+)
+
 export const request = async(config: AxiosRequestConfig): Promise<RequestReturnType> => {
   try {
     const { status, data } = await axios(config)
     return [null, { status, data }]
   }
   catch (e) {
-    return [e, null]
+    return [e.message, null]
   }
 }
